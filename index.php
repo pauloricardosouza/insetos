1acea19a64700f55c813c9b2b60681e44597dd55
Paulo Ricardo de Souza Silva <30639809+pauloricardosouza@users.noreply.github.com>
2024-03-14 16:14:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insetos TB</title>
+
+    <!-- Última versão compilada do CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Última versão compilada do JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- CDN para o Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Black+And+White+Picture&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    
+    <div class="container-fluid text-dark text-center">
+        <img src="img/logo_joaninha.png" width="100"> 
+        <h1 style="font-family:Black And White Picture">Insetos TB</h1>
+    </div>
+
+</body>
+</html>
